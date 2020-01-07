a0d78bad896dd8c7ccbcc5d411a31ece3bf56f9d
hkoketsu <hiroki.koketsu@gmail.com>
2020-01-07 14:05:32 -0800

    Update the generated pages

--- Web/razer-pages-movie/razer-pages-movie/Models/Movie.cs
+++ Web/razer-pages-movie/razer-pages-movie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace razer_pages_movie.Models
 {
@@ -8,9 +9,12 @@ namespace razer_pages_movie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
