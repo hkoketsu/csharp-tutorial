b92e3e54a7c2951f8677de031c5fdfeba08f3890
hkoketsu <hiroki.koketsu@gmail.com>
2020-01-07 18:01:13 -0800

    Add a new field to Movie table and migrate

--- Web/razer-pages-movie/razer-pages-movie/Migrations/20200108015826_Rating.Designer.cs
+++ Web/razer-pages-movie/razer-pages-movie/Migrations/20200108015826_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using razer_pages_movie.Data;
 
 namespace razer_pages_movie.Migrations
 {
     [DbContext(typeof(RazerPagesMovieContext))]
-    partial class RazerPagesMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200108015826_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
